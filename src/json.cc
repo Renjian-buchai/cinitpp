edf0e6907e365e95b94df3dc1659bb009b98226a
Renjian-buchai <tyiwei0@gmail.com>
2023-10-23 23:05:29 +0800

    Fix: #6

--- src/json.cc
+++ src/json.cc
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <regex>
 
 #include "../include/enum.hh"
 
@@ -28,12 +29,23 @@ uint8_t configOut(std::vector<directoryItem>& dirStructure,
   configStr += "   \"" + dirStructure.crbegin()->name.string() + "\": \"" +
                dirStructure.crbegin()->contents + "\"\n}";
 
+  // for (auto i = configStr.begin(); i != configStr.end(); ++i) {
+  //   if (*i == '\\') {
+  //     *i = 0x07;
+  //   }
+  // }
+  // configStr = std::regex_replace(
+  //     configStr, std::regex(std::to_string((char)0x07)), "\\\\");
+
+  configStr = std::regex_replace(configStr, std::regex("\\\\"), "\\\\");
+
+  std::cout << configStr;
+  config << configStr;
   if (!json::accept(configStr)) {
     std::cerr << "Somehow the config is not valid JSON.\n";
     return exitVal::invalidJSON;
   }
 
-  config << configStr;
   config.close();
   return exitVal::success;
 }
