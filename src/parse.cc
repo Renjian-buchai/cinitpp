d1043af1592320e231b6f696606716f36499c149
Renjian-buchai <tyiwei0@gmail.com>
2023-10-24 11:15:37 +0800

    Chore: Removed dead code. #5

--- src/parse.cc
+++ src/parse.cc
@@ -31,47 +31,14 @@ int ParseDir(std::filesystem::path dirToParse,
         static_cast<std::filesystem::path>(
             ("." / std::filesystem::relative(entry, dirToParse)).string() +
 #if defined(_WIN32) || defined(_WIN64)
-            "\\"),
+            "\\"
 #else
-            "/"),
+            "/"
 #endif
+            ),
       });
     }
   }
 
-  return exitVal::success;
-}
-
-int ParseItem(std::filesystem::path itemToParse, std::filesystem::path srcDir,
-              std::vector<directoryItem>& target, std::error_code& err) {
-  uint8_t errNumber;
-  if (!std::filesystem::is_directory(itemToParse)) {
-    target.push_back({});
-    target.back().name = "." / std::filesystem::relative(itemToParse, srcDir);
-    std::ifstream file{itemToParse};
-    if (!file.is_open()) {
-      return exitVal::parseFileOpeningFailed;
-    }
-    target.back().contents = std::string(std::istreambuf_iterator<char>(file),
-                                         std::istreambuf_iterator<char>());
-    file.close();
-
-  } else {  // is a directory
-    if (std::filesystem::is_empty(itemToParse)) {
-      target.push_back({});
-      target.back().name =
-          "." / (std::filesystem::relative(itemToParse, srcDir)) / "";
-      target.back().contents = "";
-
-    } else {
-      for (const auto& entry :
-           std::filesystem::directory_iterator(itemToParse)) {
-        errNumber = ParseItem(entry.path(), srcDir, target, err);
-        if (errNumber) {
-          return errNumber;
-        }
-      }
-    }
-  }
   return exitVal::success;
 }
